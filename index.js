337bfb3fcbac763e055b9f0aa52ede267ca9ed74
Gino Desangles <blushingbox@yahoo.com>
2019-05-12 23:55:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ document.querySelector("main#main").remove();
 
 const newHeader = document.createElement("h1");
 newHeader.id = "victory";
-newHeader.innerHTML = "Every Mod3 student is the champion";
+newHeader.innerHTML = "is the champion";
 document.body.appendChild(newHeader);
